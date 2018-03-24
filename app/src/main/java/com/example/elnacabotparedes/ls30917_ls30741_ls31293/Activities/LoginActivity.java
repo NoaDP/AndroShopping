a64733002900ca6f1feab1112552561af8d137f9
NoaDP <n.duran.plass@gmail.com>
2018-03-24 17:29:04 +0100

    se ha aplicado la configuración del login

--- app/src/main/java/com/example/elnacabotparedes/ls30917_ls30741_ls31293/Activities/LoginActivity.java
+++ app/src/main/java/com/example/elnacabotparedes/ls30917_ls30741_ls31293/Activities/LoginActivity.java
@@ -6,6 +6,9 @@ import android.content.Intent;
 import android.widget.Button;
 import android.view.View.OnClickListener;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
 
 
 import com.example.elnacabotparedes.ls30917_ls30741_ls31293.R;
@@ -13,21 +16,38 @@ import com.example.elnacabotparedes.ls30917_ls30741_ls31293.R;
 public class LoginActivity extends AppCompatActivity {
 
     Button entrar;
+    String email;
+    String password;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        getSupportActionBar().hide();
+
         entrar = (Button) findViewById(R.id.entrar);
 
         entrar.setOnClickListener(new OnClickListener() {
+
             public void onClick(View arg0) {
-                Open();
+
+                EditText iemail = (EditText)findViewById(R.id.introemail);
+                email = iemail.getText().toString();
+                EditText ipassword = (EditText)findViewById(R.id.intropassword);
+                password = ipassword.getText().toString();
+
+                if(email.equals("admin@salleurl.edu") && password.equals("qwerty123")){
+                    Open();
+                }else {
+                    Toast toast = Toast.makeText(getApplicationContext(), "FUNCIONA", Toast.LENGTH_SHORT);
+                    toast.show();
+                }
             }
         });
     }
-
-
+    
     public void Open(){
         Intent intent = new Intent(this, BuyActivity.class);
         startActivity(intent);
