c351308e3c7a1601b8bb5a7f90ff4152aedb77a8
Elna Cabot Paredes <ls30917@salleurl.edu>
2018-03-24 17:30:57 +0100

    Login configurado ( Falta poner en alguna parte los valores por defecto)

--- app/src/main/java/com/example/elnacabotparedes/ls30917_ls30741_ls31293/Activities/LoginActivity.java
+++ app/src/main/java/com/example/elnacabotparedes/ls30917_ls30741_ls31293/Activities/LoginActivity.java
@@ -1,29 +1,39 @@
 package com.example.elnacabotparedes.ls30917_ls30741_ls31293.Activities;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.content.Intent;
-import android.widget.Button;
-import android.view.View.OnClickListener;
 import android.view.View;
-
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.example.elnacabotparedes.ls30917_ls30741_ls31293.R;
 
 public class LoginActivity extends AppCompatActivity {
 
-    Button entrar;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        entrar = (Button) findViewById(R.id.entrar);
+        getSupportActionBar().hide();
+        Button entrar = (Button) findViewById(R.id.entrar);
 
-        entrar.setOnClickListener(new OnClickListener() {
-            public void onClick(View arg0) {
-                Open();
-            }
+        entrar.setOnClickListener(new View.OnClickListener() {
+              @Override
+              public void onClick(View view) {
+                  EditText iemail = (EditText)findViewById(R.id.introemail);
+                  String email = iemail.getText().toString();
+                  EditText ipassword = (EditText)findViewById(R.id.intropassword);
+                  String password = ipassword.getText().toString();
+
+                  if(email.equals("admin@salleurl.edu") && password.equals("qwerty123")){
+                      Open();
+                  }else{
+                      Toast toast = Toast.makeText(getApplicationContext(), "FUNCIONA", Toast.LENGTH_SHORT);
+                      toast.show();
+                  }
+              }
         });
     }
 
@@ -33,4 +43,5 @@ public class LoginActivity extends AppCompatActivity {
         startActivity(intent);
 
     }
+
 }
